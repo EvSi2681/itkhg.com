e4542c869aa860c75fac6c95a74e4699e8ad674a
Anatoly <tereshinaa@tngc.ru>
2025-10-12 09:38:20 +0300

    Использование доменного имени вместо ip

--- chat.js
+++ chat.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const API_URL = "http://87.121.38.23:3000/chat/send"; {
+    const API_URL = "http://itkhg.com/chat/send"; {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(req.body)
